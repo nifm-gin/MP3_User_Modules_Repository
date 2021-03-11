3e8823d13a00cb3686093c3740d4a8fab801afab
Benjamin Lemasson <benjamin.lemasson@univ-grenoble-alpes.fr>
2021-03-11 16:24:02 +0100

    NEW Module !!  Module_Merge_scans is useful if you want to merge two scans

---
+++ Spatial/Module_Merge_scans.m
@@ -0,0 +1,187 @@
+function [files_in,files_out,opt] = Module_Merge_scans(files_in,files_out,opt)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Initialization and syntax checks %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Initialize the module's parameters with default values
+if isempty(opt)
+    %
+    %     %%   % define every option needed to run this module
+    %     % --> module_option(1,:) = field names
+    %     % --> module_option(2,:) = defaults values
+    module_option(:,1)   = {'folder_out',''};
+    module_option(:,2)   = {'flag_test',true};
+    module_option(:,3)   = {'OutputSequenceName','AllName'};
+    module_option(:,4)   = {'RefInput',1};
+    module_option(:,5)   = {'InputToReshape',1};
+    module_option(:,6)   = {'Table_in', table()};
+    module_option(:,7)   = {'Table_out', table()};
+    module_option(:,8)   = {'output_filename','Merged_scaned'};
+    module_option(:,9)   = {'Output_orientation','First input'};
+    
+    
+    
+    
+    opt.Module_settings = psom_struct_defaults(struct(),module_option(1,:),module_option(2,:));
+    %
+    %      additional_info_name = {'Operation', 'Day of the 2nd scan', 'Name of the 2nd scan'};
+    %         additional_info_data = {'Addition', 'first', handles.Math_parameters_list{1}};
+    %         additional_info_format = {{'Addition', 'Subtraction', 'Multiplication', 'Division', 'Percentage', 'Concentration'},...
+    %             ['first', '-1', '+1',  'same', handles.Math_tp_list], handles.Math_parameters_list};
+    %         additional_info_editable = [1 1 1];
+    
+    
+        %% list of everything displayed to the user associated to their 'type'
+         % --> user_parameter(1,:) = user_parameter_list
+         % --> user_parameter(2,:) = user_parameter_type
+         % --> user_parameter(3,:) = parameter_default
+         % --> user_parameter(4,:) = psom_parameter_list
+         % --> user_parameter(5,:) = Scans_input_DOF : Degrees of Freedom for the user to choose the scan
+         % --> user_parameter(6,:) = IsInputMandatoryOrOptional : If none, the input is set as Optional. 
+         % --> user_parameter(7,:) = Help : text data which describe the parameter (it
+         % will be display to help the user)
+    user_parameter(:,1)   = {'Description','Text','','','', '',...
+        {'Description of the module : this module merge 2 scans in order to generate one fused image'}}  ;
+    user_parameter(:,2)   = {'Select the first scan','1Scan','','',{'SequenceName'}, 'Mandatory',''};
+    user_parameter(:,3)   = {'Select the second scan','1Scan','','',{'SequenceName'}, 'Mandatory',''};
+    user_parameter(:,4)   = {'   .Output filename','char','Merged_scaned','output_filename','','',...
+        {'Specify the name of the output scan.'
+        'Default filename is ''Merged_scaned''.'}'};
+    user_parameter(:,5)   = {'   .Output orientation','cell',{'First input', 'Second input'},'Output_orientation','','',...
+        {'Specify the output orientation/resolution'
+        '--> Output orienation = First input'
+        '--> Output orientation = Second input'
+        }'};
+    VariableNames = {'Names_Display', 'Type', 'Default', 'PSOM_Fields', 'Scans_Input_DOF', 'IsInputMandatoryOrOptional','Help'};
+    opt.table = table(user_parameter(1,:)', user_parameter(2,:)', user_parameter(3,:)', user_parameter(4,:)', user_parameter(5,:)', user_parameter(6,:)', user_parameter(7,:)','VariableNames', VariableNames);
+    %%
+    
+    % So for no input file is selected and therefore no output
+    % The output file will be generated automatically when the input file
+    % will be selected by the user
+    files_in = {''};
+    files_out = {''};
+    return
+    
+end
+%%%%%%%%
+
+if isempty(files_out)
+    opt.Table_out = opt.Table_in(opt.RefInput,:);
+    opt.Table_out.IsRaw = categorical(0);   
+    opt.Table_out.Path = categorical(cellstr([opt.folder_out, filesep]));
+    if strcmp(opt.OutputSequenceName, 'AllName')
+        opt.Table_out.SequenceName = categorical(cellstr(opt.output_filename));
+    elseif strcmp(opt.OutputSequenceName, 'Extension')
+        opt.Table_out.SequenceName = categorical(cellstr([char(opt.Table_out.SequenceName), opt.output_filename]));
+    end
+    opt.Table_out.Filename = categorical(cellstr([char(opt.Table_out.Patient), '_', char(opt.Table_out.Tp), '_', char(opt.Table_out.SequenceName)]));
+    f_out = [char(opt.Table_out.Path), char(opt.Table_out.Patient), '_', char(opt.Table_out.Tp), '_', char(opt.Table_out.SequenceName), '.nii'];
+    files_out.In1{1} = f_out;
+end
+
+
+
+
+
+%% Syntax
+if ~exist('files_in','var')||~exist('files_out','var')||~exist('opt','var')
+    error('Module_Merge_scans:brick','Bad syntax, type ''help %s'' for more info.',mfilename)
+end
+
+
+%% If the test flag is true, stop here !
+
+if opt.flag_test == 1
+    return
+end
+
+
+[Status, Message, Wrong_File] = Check_files(files_in);
+if ~Status
+    error('Problem with the input file : %s \n%s', Wrong_File, Message)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% The core of the brick starts here %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% load input Nii file
+input(1).nifti_header = spm_vol(files_in.In1{1});
+input(2).nifti_header = spm_vol(files_in.In2{1});
+
+J1 = spm_jsonread(strrep(files_in.In1{1}, '.nii', '.json'));
+J2 = spm_jsonread(strrep(files_in.In2{1}, '.nii', '.json'));
+
+
+if strcmp(opt.Output_orientation, 'First input')   
+    ref_scan = 1;
+else
+    ref_scan = 2;
+end
+world_space_header = input(ref_scan).nifti_header;
+world_space_header.mat = min(input(1).nifti_header.mat, input(2).nifti_header.mat); 
+new_dim = ((input(1).nifti_header.dim(3)*input(1).nifti_header.mat(3,3)+input(2).nifti_header.dim(3)*input(2).nifti_header.mat(3,3)) / ...
+    min(input(1).nifti_header.mat(3,3), input(2).nifti_header.mat(3,3)));
+
+world_space_header.dim = max(input(1).nifti_header.dim, input(2).nifti_header.dim);
+world_space_header.dim(3) = ceil(new_dim);
+
+scan_1 = read_volume(input(1).nifti_header, world_space_header, 0);
+scan_2 = read_volume(input(2).nifti_header,world_space_header, 0);
+
+Mask1 = (scan_1 ~=0);
+Mask2 = (scan_2 ~=0);
+
+merged_scan = (scan_1.*Mask1+scan_2.*Mask2)./(Mask1+Mask2);
+
+
+
+% transform the OutputImages matrix in order to match to the nii header of the
+% first input (rotation/translation)
+
+
+if ~exist('OutputImages_reoriented', 'var')
+    OutputImages_reoriented = write_volume(merged_scan, world_space_header, 'axial');
+else
+    OutputImages_reoriented = OutputImages;
+end
+
+
+
+% save the new files (.nii & .json)
+% update the header before saving the new .nii
+info = niftiinfo(files_in.(['In' num2str(ref_scan)]){1});
+
+
+info2 = info;
+info2.Filename = files_out.In1{1};
+info2.Filemoddate = char(datetime('now'));
+info2.Datatype = class(OutputImages_reoriented);
+info2.PixelDimensions = info.PixelDimensions(1:length(size(OutputImages_reoriented)));
+info2.ImageSize = size(OutputImages_reoriented);
+info2.Description = [info.Description, 'Modified by Module Merge scans'];
+info2.Transform.T = world_space_header.mat';
+
+
+% save the new .nii file
+niftiwrite(OutputImages_reoriented, files_out.In1{1}, info2);
+
+% % so far copy the .json file of the first input
+% copyfile(strrep(files_in.In1{1}, '.nii', '.json'), strrep(files_out.In1{1}, '.nii', '.json'))
+
+%% Json Processing
+[path, name, ~] = fileparts(files_in.In1{1});
+jsonfile = [path, '/', name, '.json'];
+J = ReadJson(jsonfile);
+
+J = KeepModuleHistory(J, struct('files_in', files_in, 'files_out', files_out, 'opt', opt, 'ExecutionDate', datestr(datetime('now'))), mfilename); 
+
+[path, name, ~] = fileparts(files_out.In1{1});
+jsonfile = [path, '/', name, '.json'];
+WriteJson(J, jsonfile)
+
+
+
+
+% 
